bf75d18da11b78a2e30f955cf97f61a1e8613bbd
manisha <manishajha0512@gmail.com>
2025-04-17 11:50:03 +0530

    Removed env

--- src/app/listdata/page.tsx
+++ src/app/listdata/page.tsx
@@ -19,7 +19,7 @@ const QueryCards = () => {
 
     useEffect(() => {
         axios
-            .get(`${process.env.NEXT_PUBLIC_BASE_URL}/v0/queries/all`)
+            .get(`${process.env.NEXT_PUBLIC_BASE_URL}/queries/all`)
             .then((response) => {
                 setData(response.data);
                 setLoading(false);
